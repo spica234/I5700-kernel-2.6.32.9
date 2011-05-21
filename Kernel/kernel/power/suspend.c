31090253818cfa6b909cb45f2046c4d23a08fdf8
spica234 <adorable.boy.gujarat@gmail.com>
2011-05-21 18:01:19 +0530

    800 MHz frequency lock removed

--- Kernel/kernel/power/suspend.c
+++ Kernel/kernel/power/suspend.c
@@ -16,7 +16,6 @@
 #include <linux/cpu.h>
 #include <linux/syscalls.h>
 #include <linux/cpufreq.h>
-#include <plat/s3c64xx-dvfs.h>
 #include "power.h"
 
 const char *const pm_states[PM_SUSPEND_MAX] = {
@@ -131,7 +130,6 @@ static int suspend_enter(suspend_state_t state)
 	int error;
 
 #ifdef CONFIG_CPU_FREQ
-	dvfs_set_max_freq_lock();
 #endif /* CONFIG_CPU_FREQ */
 	if (suspend_ops->prepare) {
 		error = suspend_ops->prepare();
@@ -171,7 +169,6 @@ static int suspend_enter(suspend_state_t state)
 	arch_suspend_enable_irqs();
 	BUG_ON(irqs_disabled());
 #ifdef CONFIG_CPU_FREQ
-	dvfs_set_max_freq_unlock();       
 #endif /* CONFIG_CPU_FREQ */
 
  Enable_cpus:
